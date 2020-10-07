62b2e06777d0737d1efae1a3f5818d73554b2a04
BG00924 <BG00924@users.noreply.github.com>
2020-10-06 19:49:09 -0500

    lesson 3 complete

---
+++ assets/js/events.js
@@ -0,0 +1,35 @@
+// allows bootstrap drop down then domMethods to be used
+require("bootstrap");
+const createEl = require("./domMethods");
+//pulls in text generator
+const createLoremIpsum = require("./helpers");
+
+//This previously checkted to see if current page for events.html and applied
+// correct DOM manipulation
+// if (window.location.href.indexOf("event") > -1) {
+//This replaces that and links the DOM manipulation to each HTML page
+$(document).ready(function() {
+    const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
+        title: "Title Placeholder",
+        subtitle: "",
+        description: ""
+    };
+
+    const pageEl = document.querySelector("#page");
+    
+    const containerEl = createEl("div", {class: "container"},
+      createEl("div", {class: "card mb-3"}, 
+        createEl("img", {class: "card-img-top", style: "width: 5px", src: currentEvent.image || "https://via.placeholder.com/350x150"}),
+        createEl("div", {class: "card-body"}, 
+          createEl("h1", {class: "card-title"}, currentEvent.title || ""),
+          createEl("h2", {class: "text-muted"}, currentEvent.subtitle || ""),
+          createEl("p", {class: "card-text mt-3"}, currentEvent.description || createLoremIpsum(100)),
+          createEl("a", {class: "btn btn-primary", href: "tickets.html"}, "Buy Tickets")
+        )
+      ),
+      
+    )
+    pageEl.appendChild(containerEl)
+  }
+
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,57 @@
 const path = require("path")
 // enables built in webpack methods (i.e. provideplugin)
 const webpack = require("webpack")
+// imports analyzer
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 
 
 module.exports = {
     // root of bundle and beginning of dependency graph
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     // directs bundled code to folder we designate
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist'
+    },
+    // necessary to optimize and emit images
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i, //regex to find jpgs
+                use:[
+                    {
+                        //only emits the files
+                        loader: 'file-loader', //actual loader implementation
+                        options: {
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     // enables webpack to recognize global variable $ used by jquery
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to HTML file in the dist folder
         })
     ],
     // determines mode webpack with run (i.e. production, development, etc)
